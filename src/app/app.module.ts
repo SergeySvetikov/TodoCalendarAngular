87026444207b5d28f1683e06b9623f6931599e27
Sergey Svetikov <serikomg123@gmail.com>
2023-05-17 15:49:11 +0300

    initial calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+import { SelectorComponent } from './selector/selector.component';
+import {MomentPipe} from "./shared/moment.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    OrganizerComponent,
+    SelectorComponent,
+    MomentPipe
   ],
   imports: [
     BrowserModule,
